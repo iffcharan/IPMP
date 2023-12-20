3ad7adb86e3c9e6ed8af29b8fc2c3b3920b04761
iffcharan <131170389+iffcharan@users.noreply.github.com>
2023-12-20 22:22:04 +0530

    Create max_AND.cpp

---
+++ week1/max_AND.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+ 
+class Solution
+{
+    public:
+    // Function for finding maximum AND value.
+    int maxAND (int arr[], int n)
+    {
+        // Your code here
+        int ans = 0;
+        vector<int> vis(n,0);
+        for(int i = 31;i>=0;i--){
+            int ctone = 0;
+            for(int j = 0;j<n;j++){
+                if(vis[j]==0 and (((arr[j]>>i)&1) != 0)) ctone++;
+            }
+            if(ctone >= 2){
+                ans  = ans | (1<<i);
+                for(int j = 0;j<n;j++){
+                    if(((arr[j]>>i)&1) == 0){
+                        vis[j] = 1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+ 
+// Driver function
+int main()
+{
+    int t;
+    cin>>t;//testcases
+    while(t--)
+    {
+        int n;
+        cin>>n;//input n
+        int arr[n+5],i;
+        
+        //inserting elements
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        //calling maxAND() function
+        cout <<  obj.maxAND(arr,n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
